b9acb1b03b882871293758223eeebbef78dd6749
Sophie Khounlo <s.khounlo@utexas.edu>
2016-04-18 15:22:17 -0500

    added sBoxes, finished subBytes() and its inverse, added mixColumns(), and Rcon table

--- Assignment 5/AES.java
+++ Assignment 5/AES.java
@@ -2,7 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class AES{
-	private static int[] sBox = {
+    /* Tables for subBytes transformation.
+     */
+    final static int[] sBox = {
 		0x63,0x7C,0x77,0x7B,0xF2,0x6B,0x6F,0xC5,0x30,0x01,0x67,0x2B,0xFE,0xD7,0xAB,0x76,
 		0xCA,0x82,0xC9,0x7D,0xFA,0x59,0x47,0xF0,0xAD,0xD4,0xA2,0xAF,0x9C,0xA4,0x72,0xC0,
 		0xB7,0xFD,0x93,0x26,0x36,0x3F,0xF7,0xCC,0x34,0xA5,0xE5,0xF1,0x71,0xD8,0x31,0x15,
@@ -21,7 +23,7 @@ public class AES{
 		0x8C,0xA1,0x89,0x0D,0xBF,0xE6,0x42,0x68,0x41,0x99,0x2D,0x0F,0xB0,0x54,0xBB,0x16
 	};
 
-	private static int[] invSBox = {
+	final static int[] invSBox = {
 		0x52,0x09,0x6A,0xD5,0x30,0x36,0xA5,0x38,0xBF,0x40,0xA3,0x9E,0x81,0xF3,0xD7,0xFB,
 		0x7C,0xE3,0x39,0x82,0x9B,0x2F,0xFF,0x87,0x34,0x8E,0x43,0x44,0xC4,0xDE,0xE9,0xCB,
 		0x54,0x7B,0x94,0x32,0xA6,0xC2,0x23,0x3D,0xEE,0x4C,0x95,0x0B,0x42,0xFA,0xC3,0x4E,
@@ -40,25 +42,43 @@ public class AES{
 		0x17,0x2B,0x04,0x7E,0xBA,0x77,0xD6,0x26,0xE1,0x69,0x14,0x63,0x55,0x21,0x0C,0x7D
 	};
 
-    public static void subBytes(){ //encryption
-
+    /* Takes in the state array and uses its value as an index into a lookup
+       table, and replaces its value in state with the value stored in the table.
+       Encryption.
+     */
+    public static void subBytes(byte[][] state){
+        for(int c = 0; c < 4; c++){
+            for(int r = 0; r < 4; r++){
+                int index = (state[r][c]+256)%256;
+                state[r][c] = (byte)sBox[index];
+            }
+        }
     }
 
-    public static void invSubBytes(){ //decryption
-
+    /* Takes in the state array and uses its value as an index into a lookup
+       table, and replaces its value in state with the value stored in the table.
+       Decryption.
+     */
+    public static void invSubBytes(byte[][] state){
+        for(int c = 0; c < 4; c++){
+            for(int r = 0; r < 4; r++){
+                int index = (state[r][c]+256)%256;
+                state[r][c] = (byte)invSBox[index];
+            }
+        }
     }
 
     /* Shift row left by i bytes. i denotes the row in state.
      */
-    public static void shiftRows(byte[][] input){
+    public static void shiftRows(byte[][] state){
         for(int r = 0; r < 4; r++){
             int rownum = r;
             while(rownum-- != 0){
-                byte temp = input[r][0];
+                byte temp = state[r][0];
                 for(int c = 0; c < 3; c++){
-                    input[r][c] = input[r][c+1];
+                    state[r][c] = state[r][c+1];
                 }
-                input[r][3] = temp;
+                state[r][3] = temp;
             }
         }
     }
@@ -66,20 +86,111 @@ public class AES{
     /* Inverse of shiftRows(), returns the matrix to original matrix.
      * Shift row right by i bytes. i denotes the row in state.
      */
-    public static void invShiftRows(byte[][] input){
+    public static void invShiftRows(byte[][] state){
         for(int r = 0; r < 4; r++){
             int rownum = r;
             while(rownum-- != 0){
-                byte temp = input[r][3];
+                byte temp = state[r][3];
                 for(int c = 3; c > 0; c--){
-                    input[r][c] = input[r][c-1];
+                    state[r][c] = state[r][c-1];
                 }
-                input[r][0] = temp;
+                state[r][0] = temp;
             }
         }
     }
 
-    private static int[][] Rcon = { {0x01, 0x00, 0x00, 0x00},   // col 0
+   ////////////////////////  the mixColumns Tranformation ////////////////////////
+   final static int[] LogTable = {
+   0,   0,  25,   1,  50,   2,  26, 198,  75, 199,  27, 104,  51, 238, 223,   3,
+   100,   4, 224,  14,  52, 141, 129, 239,  76, 113,   8, 200, 248, 105,  28, 193,
+   125, 194,  29, 181, 249, 185,  39, 106,  77, 228, 166, 114, 154, 201,   9, 120,
+   101,  47, 138,   5,  33,  15, 225,  36,  18, 240, 130,  69,  53, 147, 218, 142,
+   150, 143, 219, 189,  54, 208, 206, 148,  19,  92, 210, 241,  64,  70, 131,  56,
+   102, 221, 253,  48, 191,   6, 139,  98, 179,  37, 226, 152,  34, 136, 145,  16,
+   126, 110,  72, 195, 163, 182,  30,  66,  58, 107,  40,  84, 250, 133,  61, 186,
+   43, 121,  10,  21, 155, 159,  94, 202,  78, 212, 172, 229, 243, 115, 167,  87,
+   175,  88, 168,  80, 244, 234, 214, 116,  79, 174, 233, 213, 231, 230, 173, 232,
+   44, 215, 117, 122, 235,  22,  11, 245,  89, 203,  95, 176, 156, 169,  81, 160,
+   127,  12, 246, 111,  23, 196,  73, 236, 216,  67,  31,  45, 164, 118, 123, 183,
+   204, 187,  62,  90, 251,  96, 177, 134,  59,  82, 161, 108, 170,  85,  41, 157,
+   151, 178, 135, 144,  97, 190, 220, 252, 188, 149, 207, 205,  55,  63,  91, 209,
+   83,  57, 132,  60,  65, 162, 109,  71,  20,  42, 158,  93,  86, 242, 211, 171,
+   68,  17, 146, 217,  35,  32,  46, 137, 180, 124, 184,  38, 119, 153, 227, 165,
+   103,  74, 237, 222, 197,  49, 254,  24,  13,  99, 140, 128, 192, 247, 112,   7};
+
+   final static int[] AlogTable = {
+   1,   3,   5,  15,  17,  51,  85, 255,  26,  46, 114, 150, 161, 248,  19,  53,
+   95, 225,  56,  72, 216, 115, 149, 164, 247,   2,   6,  10,  30,  34, 102, 170,
+   229,  52,  92, 228,  55,  89, 235,  38, 106, 190, 217, 112, 144, 171, 230,  49,
+   83, 245,   4,  12,  20,  60,  68, 204,  79, 209, 104, 184, 211, 110, 178, 205,
+   76, 212, 103, 169, 224,  59,  77, 215,  98, 166, 241,   8,  24,  40, 120, 136,
+   131, 158, 185, 208, 107, 189, 220, 127, 129, 152, 179, 206,  73, 219, 118, 154,
+   181, 196,  87, 249,  16,  48,  80, 240,  11,  29,  39, 105, 187, 214,  97, 163,
+   254,  25,  43, 125, 135, 146, 173, 236,  47, 113, 147, 174, 233,  32,  96, 160,
+   251,  22,  58,  78, 210, 109, 183, 194,  93, 231,  50,  86, 250,  21,  63,  65,
+   195,  94, 226,  61,  71, 201,  64, 192,  91, 237,  44, 116, 156, 191, 218, 117,
+   159, 186, 213, 100, 172, 239,  42, 126, 130, 157, 188, 223, 122, 142, 137, 128,
+   155, 182, 193,  88, 232,  35, 101, 175, 234,  37, 111, 177, 200,  67, 197,  84,
+   252,  31,  33,  99, 165, 244,   7,   9,  27,  45, 119, 153, 176, 203,  70, 202,
+   69, 207,  74, 222, 121, 139, 134, 145, 168, 227,  62,  66, 198,  81, 243,  14,
+   18,  54,  90, 238,  41, 123, 141, 140, 143, 138, 133, 148, 167, 242,  13,  23,
+   57,  75, 221, 124, 132, 151, 162, 253,  28,  36, 108, 180, 199,  82, 246,   1};
+
+   private byte mul (int a, byte b) {
+   int inda = (a < 0) ? (a + 256) : a;
+   int indb = (b < 0) ? (b + 256) : b;
+
+   if ( (a != 0) && (b != 0) ) {
+       int index = (LogTable[inda] + LogTable[indb]);
+       byte val = (byte)(AlogTable[ index % 255 ] );
+       return val;
+   }
+   else
+       return 0;
+   } // mul
+
+   // In the following two methods, the input c is the column number in
+   // your evolving state matrix st (which originally contained
+   // the plaintext input but is being modified).  Notice that the state here is defined as an
+   // array of bytes.  If your state is an array of integers, you'll have
+   // to make adjustments.
+
+   public void mixColumn2 (int c) {
+   // This is another alternate version of mixColumn, using the
+   // logtables to do the computation.
+
+   byte a[] = new byte[4];
+
+   // note that a is just a copy of st[.][c]
+   for (int i = 0; i < 4; i++)
+       a[i] = st[i][c];
+
+   // This is exactly the same as mixColumns1, if
+   // the mul columns somehow match the b columns there.
+   st[0][c] = (byte)(mul(2,a[0]) ^ a[2] ^ a[3] ^ mul(3,a[1]));
+   st[1][c] = (byte)(mul(2,a[1]) ^ a[3] ^ a[0] ^ mul(3,a[2]));
+   st[2][c] = (byte)(mul(2,a[2]) ^ a[0] ^ a[1] ^ mul(3,a[3]));
+   st[3][c] = (byte)(mul(2,a[3]) ^ a[1] ^ a[2] ^ mul(3,a[0]));
+   } // mixColumn2
+
+   public void invMixColumn2 (int c) {
+   byte a[] = new byte[4];
+
+   // note that a is just a copy of st[.][c]
+   for (int i = 0; i < 4; i++)
+       a[i] = st[i][c];
+
+   st[0][c] = (byte)(mul(0xE,a[0]) ^ mul(0xB,a[1]) ^ mul(0xD, a[2]) ^ mul(0x9,a[3]));
+   st[1][c] = (byte)(mul(0xE,a[1]) ^ mul(0xB,a[2]) ^ mul(0xD, a[3]) ^ mul(0x9,a[0]));
+   st[2][c] = (byte)(mul(0xE,a[2]) ^ mul(0xB,a[3]) ^ mul(0xD, a[0]) ^ mul(0x9,a[1]));
+   st[3][c] = (byte)(mul(0xE,a[3]) ^ mul(0xB,a[0]) ^ mul(0xD, a[1]) ^ mul(0x9,a[2]));
+    } // invMixColumn2
+
+    public static void addRoundKey(byte[][] state, byte[][]roundKey){
+
+    }
+
+    final static int[][] Rcon = { {0x01, 0x00, 0x00, 0x00},   // col 0
                            {0x02, 0x00, 0x00, 0x00},    // col 1
                            {0x04, 0x00, 0x00, 0x00},    // col 2
                            {0x08, 0x00, 0x00, 0x00},    // col 3
@@ -125,7 +236,7 @@ public class AES{
     public static byte[][] to2D(byte[] array){
         byte[][] b = new byte[4][4];
         for(int i = 0; i < array.length; i++){
-            b[i/4][i%4] = array[i];
+            b[i%4][i/4] = (byte)array[i];
         }
         return b;
     }
@@ -134,16 +245,20 @@ public class AES{
      * Pads with zeroes if less than 32 Hex characters, truncates if more than.
      */
     public static byte[] hexToByte(String s){
+        byte[] key = new byte[16];
         while(s.length() < 32){
             s += "0";
         }
         if(s.length() > 32){
             s = s.substring(0, 32);
         }
-        byte[] key = new byte[16];
         for(int i = 0; i < 32; i+=2){
-            String temp = s.substring(i, i+2);
-            key[i/2] = (byte) Integer.parseInt(temp, 16);
+            key[i/2] = (byte)((Character.digit(s.charAt(i), 16) << 4)
+                        + Character.digit(s.charAt(i+1), 16));
+            System.out.println("Left: "+(Character.digit(s.charAt(i), 16)<<4));
+            System.out.println("Right: "+(Character.digit(s.charAt(i+1), 16)));
+            System.out.println( ((Character.digit(s.charAt(i), 16) << 4)
+                        + Character.digit(s.charAt(i+1), 16)));
         }
         return key;
     }
@@ -172,10 +287,17 @@ public class AES{
 
             Scanner keyScan = new Scanner(new File(keyFile));
             String k = keyScan.nextLine();
+            // System.out.println("Keys\n");
+            byte[] keys = hexToByte(k);
+            System.out.println("\nKey\n");
             byte[][] key = to2D(hexToByte(k));
             keyScan.close();
+            // for(int b : keys){
+            //     System.out.print(b + " ");
+            // }
+            // System.out.println();
 
-            // Scanner fileScan = new Scanner(new File(file));
+            Scanner fileScan = new Scanner(new File(file));
 
             // Can actually have this in keyExpansion(). Just take in the key[][].
             byte[][] keySchedule = new byte[4][120];
@@ -190,39 +312,49 @@ public class AES{
 				{ 13, 14, 15, 0 } };
             for(int i = 0; i < 4; i++){
                 for(int j = 0; j < 4; j++){
-                    System.out.print(twoD[i][j]+" ");
+                    System.out.print(key[i][j]+" ");
                 }
                 System.out.println();
             }
             System.out.println();
-            shiftRows(twoD);
-            for(int i = 0; i < 4; i++){
-                for(int j = 0; j < 4; j++){
-                    System.out.print(twoD[i][j]+" ");
-                }
-                System.out.println();
-            }
-            System.out.println();
-            invShiftRows(twoD);
-            for(int i = 0; i < 4; i++){
-                for(int j = 0; j < 4; j++){
-                    System.out.print(twoD[i][j]+" ");
-                }
-                System.out.println();
-            }
             byte b[] = {1,2,3,4};
             rotWord(b);
             for(byte a : b){
                 System.out.println(a);
             }
+            System.out.println();
+            byte[] a = {1,2,3,4,5,6,7,8,9,10,11,12};
+            byte[][] ba = to2D(a);
+            for(int i = 0; i < 4; i++){
+                for(int j = 0; j < 4; j++){
+                    System.out.print(ba[i][j]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
 
             if(option.equals("e")){
-                // while(fileScan.hasNext()){
-                //     String line = fileScan.nextLine();
-                //     byte[][] state = to2D(hexToByte(line));
-                //     // FileWriter fw = new FileWriter(file+".enc");
-                //     // fw.close();
-                // }
+                while(fileScan.hasNext()){
+                    String line = fileScan.nextLine();
+                    System.out.println("\nState\n");
+                    byte[] states = hexToByte(line);
+                    for(int i : states){
+                        System.out.print(i+" ");
+                    }
+                    System.out.println();
+                    byte[][] state = to2D(hexToByte(line));
+                    for(int i = 0; i < 4; i++){
+                        for(int j = 0; j < 4; j++){
+                            System.out.print(state[i][j]+" ");
+                            // System.out.print("left: "+(state[i][j]/16)+" | ");
+                            // System.out.print("right: "+(state[i][j]%16)+" | ");
+                            // System.out.println("index: "+((state[i][j]+256)%256)+" | ");
+                        }
+                        System.out.println();
+                    }
+                    // FileWriter fw = new FileWriter(file+".enc");
+                    // fw.close();
+                }
             }
             else if(option.equals("d")){
                 // FileWriter fw = new FileWriter(file+".dec");
